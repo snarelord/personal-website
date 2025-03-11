837fd6e996739538115826ea4143f6bb072fa7be
Kit Jones <174150846+snarelord@users.noreply.github.com>
2025-03-11 12:06:55 +0000

    fixed meta data

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "c9d2176cb2b8d9b6"; }
+/******/ 		__webpack_require__.h = function() { return "bd14c2ae65a1bcf6"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,7 @@
+export const metadata = {
+  title: "Kit Jones - Software Engineer",
+};
+
 import Image from "next/image";
 import Header from "@/components/header";
 import ProjectItem from "@/components/project-item";
