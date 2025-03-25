95bf1796abadc0dec5775aee317e69491f21286b
Kit Jones <174150846+snarelord@users.noreply.github.com>
2025-03-25 22:59:06 +0000

    test sentry workflow

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -5,7 +5,7 @@
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-  dsn: "https://55eca4e6d06918c4825dae1485d00b33@o4508800357892096.ingest.de.sentry.io/4509040684695632",
+  dsn: process.env.SENTRY_DSN,
 
   // Add optional integrations for additional features
   integrations: [Sentry.replayIntegration()],
--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -5,8 +5,7 @@
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-  dsn: "https://55eca4e6d06918c4825dae1485d00b33@o4508800357892096.ingest.de.sentry.io/4509040684695632",
-
+  dsn: process.env.SENTRY_DSN,
   // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
   tracesSampleRate: 1,
 
