b1769e7702254eb33d98f6d66b7d83891eeddfc9
Kit Jones <174150846+snarelord@users.noreply.github.com>
2025-04-30 13:04:49 +0100

    techstack animation

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,9 @@
 import { useEffect, useRef } from "react";
 import anime from "animejs";
 import Image from "next/image";
-import Header from "@/components/Header";
-import TechStack from "@/components/TechStack";
-import ProjectItem from "@/components/ProjectItem";
+import Header from "../components/header";
+import TechStack from "../components/TechStack";
+import ProjectItem from "../components/ProjectItem";
 import styles from "./page.module.css";
 
 import { ChevronsLeftRight } from "lucide-react"; // js
